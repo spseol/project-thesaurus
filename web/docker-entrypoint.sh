81492ae8516897226c7f44e3cb0065647b082fab
Josef Kolář <thejoeejoee@gmail.com>
2020-04-20 02:19:10 +0300

    Refactor of web container startup

---
+++ web/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+run() {
+  # Start process as unprivileged user
+  # Use `exec` to replace original process.
+  # This makes it possible for Docker to send signals to the process.
+  exec su django -c "$*"
+}
+
+
+case $1 in
+  migrate)
+    # under root
+    python manage.py makemigrations && python manage.py migrate
+    ;;
+  django-admin)
+    run "$*"
+    ;;
+  runserver)
+    # runtime under django user
+    run "python manage.py collectstatic --noinput && python manage.py runserver_plus --nopin 0.0.0.0:8000"
+    ;;
+  runserver_prod)
+    # runtime under django user
+    run "gunicorn thesaurus.wsgi -b 0.0.0.0:8000"
+    ;;
+  *)
+    exec "$@"
+    exit
+    ;;
+esac
+
