8b5d627ed689ae8468102ae244762c4d96da7bbe
Josef Kolář <thejoeejoee@gmail.com>
2020-05-22 22:57:59 +0300

    Typos

--- django/apps/accounts/ldap.py
+++ django/apps/accounts/ldap.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import Callable, Any
 
 from django.contrib.auth.models import Group
 
@@ -7,13 +8,15 @@ from apps.accounts.models import User
 
 
 class keydefaultdict(defaultdict):
+    default_factory: Callable[[str], Any]
+
     # TODO: remove after feature/audit-log merge
     def __missing__(self, key):
         if self.default_factory is None:
             raise KeyError(key)
-        else:
-            ret = self[key] = self.default_factory(key)
-            return ret
+
+        ret = self[key] = self.default_factory(key)
+        return ret
 
 
 _group_cache = keydefaultdict(lambda n: Group.objects.get_or_create(name=n)[0])
