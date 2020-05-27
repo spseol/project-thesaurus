0ae4c9d9d39e7aa2706dcc7f5af67261cae544b9
Josef Kolář <thejoeejoee@gmail.com>
2020-05-27 12:45:41 +0300

    Restricted user filter only for teachers

--- django/apps/api/views/user.py
+++ django/apps/api/views/user.py
@@ -8,11 +8,13 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from apps.accounts.models import User
-from apps.accounts.models.managers import UserQueryset
 from apps.accounts.serializers import UserOptionSerializer, StudentOptionSerializer
 
 
 class UserFilterOptionsViewSet(ReadOnlyModelViewSet):
+    queryset = User.school_users.with_school_account().teachers().annotate(
+        thesis_count=Count('thesis_supervisor') + Count('thesis_opponent')
+    ).order_by('-thesis_count')
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = None
     serializer_class = UserOptionSerializer
@@ -20,18 +22,6 @@ class UserFilterOptionsViewSet(ReadOnlyModelViewSet):
         'first_name',
         'last_name',
     )
-
-    def get_queryset(self):
-        queryset = User.school_users.with_school_account().teachers()  # type: UserQueryset
-
-        queryset = queryset.annotate(
-            thesis_count=Count('thesis_supervisor') + Count('thesis_opponent')
-        ).order_by('-thesis_count')
-        # TODO: add students for role>=teacher?
-        if self.request.user.is_teacher or self.request.user.is_manager or self.request.user.is_superuser:
-            return queryset
-
-        return queryset.teachers()
 
 
 class StudentOptionsViewSet(ReadOnlyModelViewSet):
