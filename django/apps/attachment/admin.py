e8737d927aec135b0b1b48fa70d7ba9203acb2e7
Josef Kolář <thejoeejoee@gmail.com>
2021-04-13 13:07:39 +0200

    Improved attachment admin

--- django/apps/attachment/admin.py
+++ django/apps/attachment/admin.py
@@ -1,4 +1,7 @@
 from django.contrib.admin import ModelAdmin, register
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 from django_better_admin_arrayfield.admin.mixins import DynamicArrayMixin
 
 from apps.attachment.models import Attachment, TypeAttachment
@@ -6,9 +9,19 @@ from apps.attachment.models import Attachment, TypeAttachment
 
 @register(Attachment)
 class AttachmentAdmin(ModelAdmin):
-    pass
+    list_display = ['thesis', 'type_attachment', 'file_path', 'size', 'get_download_link']
+    list_filter = ['type_attachment']
+
+    def get_download_link(self, obj: Attachment):
+        return format_html(
+            '<a href="{}" target="_blank">{}</a>',
+            reverse('api:v1:attachment-detail', args=(obj.pk,)),
+            _('Download attachment')
+        )
+
+    get_download_link.short_description = _('Download attachment')
 
 
 @register(TypeAttachment)
 class TypeAttachmentAdmin(ModelAdmin, DynamicArrayMixin):
-    pass
+    list_display = ['__str__', 'allowed_content_types', 'is_public', 'max_size']
